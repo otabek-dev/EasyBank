cf8d7e8118986cf538dc8e856dc01da977c957aa
otabek231 <otabek.ru.edu@gmail.com>
2023-09-06 14:32:47 +0500

    Created employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyBank.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!; 
+        public string Position { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
